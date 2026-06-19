86970c4d2006f500a356bdeb36c1d540c992c25a
Antoine Hoorelbeke <antoine.hoorelbeke@mistral.ai>
2026-06-19 17:01:03 +0200

    fix print

--- src/worker.py
+++ src/worker.py
@@ -4,5 +4,5 @@ import os
 from entrypoints.worker import main
 
 if __name__ == "__main__":
-    print(f'{os.getenv("MISTRALAI_API_KEY")=}')
+    print(f'{os.getenv("MISTRAL_API_KEY")=}')
     asyncio.run(main())
